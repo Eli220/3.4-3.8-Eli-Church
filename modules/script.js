ea75c593d8bd4ad16f9a4060d13c3b1153982790
Eli220 <124737330+Eli220@users.noreply.github.com>
2024-08-26 04:10:57 +0000

    Both pages largely complete

--- modules/script.js
+++ modules/script.js
@@ -19,15 +19,44 @@ function hamburger() {
 
 function linksShow(id) {
     var dropDowns = document.getElementById(id);
+    console.log(id);
+    if (dropDowns.style.display === "block") {
+        dropDowns.style.display = "none";
+    }     
+    else {
+        dropDowns.style.display = "block";
+        dropDowns.style.height = "100%";
+    }
+}
+
+/** function linksShowCl(cl) {
+    var dropDowns = document.getElementsByClassName(cl);
+    console.log(cl);
     if (dropDowns.style.display === "block") {
         dropDowns.style.display = "none";
     }     
     else {
         dropDowns.style.display = "block";
     }
+}**/
+
+function linksShowCl(cl) {
+    var dropDowns = document.getElementsByClassName(cl);
+    console.log(cl);
+
+    for (var i = 0; i < dropDowns.length; i++) {
+        if (dropDowns[i].style.display === "block") {
+            dropDowns[i].style.display = "none";
+        } else {
+            dropDowns[i].style.display = "block";
+        }
+    }
 }
 
-
 function linkClick(url) {
-    window.location = url
+  window.location = url;
+}
+
+function linkClickNew(url) {
+  window.open(url, '_blank')
 }
