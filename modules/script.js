67f76bda89246341f8deecc383e07695cbf868f4
KatieLong1 <45803365+KatieLong1@users.noreply.github.com>
2024-08-27 21:27:11 +0000

    Replit issue

--- modules/script.js
+++ modules/script.js
@@ -1,12 +1,19 @@
-
-
+/********************************************
+ * mysteriously named function
+ * Search function
+ * onsubmit
+********************************************/
 document.getElementById('searchForm').onsubmit = function() {
-    console.log("plug")
+    console.log("plug");
     window.location = 'http://www.google.com/search?q=site:ivvseztzqq-2403522582-a.codehs.me/index.html ' + 
 document.getElementById('searchTxt').value;
     return false;
 }
-
+/********************************************
+ * hamburger()
+ * Hamburger Dropdown
+ * onclick
+********************************************/
 function hamburger() {
     var dropDowns = document.getElementById("drop-down-links");
     if (dropDowns.style.display === "block") {
@@ -16,10 +23,14 @@ function hamburger() {
         dropDowns.style.display = "block";
     }
 }
-
-function linksShow(id) {
-    var dropDowns = document.getElementById(id);
-    console.log(id);
+/********************************************
+ * linksShow( id of links )
+ * Show links with '_id'
+ * onclick
+********************************************/
+function linksShow(_id) {
+    var dropDowns = document.getElementById(_id);
+    console.log(_id);
     if (dropDowns.style.display === "block") {
         dropDowns.style.display = "none";
     }     
@@ -28,22 +39,14 @@ function linksShow(id) {
         dropDowns.style.height = "100%";
     }
 }
-
-/** function linksShowCl(cl) {
-    var dropDowns = document.getElementsByClassName(cl);
-    console.log(cl);
-    if (dropDowns.style.display === "block") {
-        dropDowns.style.display = "none";
-    }     
-    else {
-        dropDowns.style.display = "block";
-    }
-}**/
-
-function linksShowCl(cl) {
-    var dropDowns = document.getElementsByClassName(cl);
-    console.log(cl);
-
+/********************************************
+ * linksShowCl( class of links )
+ * Show links by class with '_cl'
+ * onclick
+********************************************/
+function linksShowCl(_cl) {
+    var dropDowns = document.getElementsByClassName(_cl);
+    console.log(_cl);
     for (var i = 0; i < dropDowns.length; i++) {
         if (dropDowns[i].style.display === "block") {
             dropDowns[i].style.display = "none";
@@ -52,11 +55,20 @@ function linksShowCl(cl) {
         }
     }
 }
-
+/********************************************
+ * linkClick( url to go to )
+ * Functions as a link
+ * onclick
+********************************************/
 function linkClick(url) {
   window.location = url;
 }
-
+/********************************************
+ * linkClickNew( url to go to )
+ * Opens link in new tab
+ * onclick
+********************************************/
 function linkClickNew(url) {
-  window.open(url, '_blank')
+  window.open(url, '_blank');
 }
+/***************** END OF JS :) *****************/
